0e514aef6c5e0a281c14639a9354c529652dab41
Victor Alonso <victor@ieselcaminas.org>
2018-11-18 13:52:29 +0100

    Initial Commit

---
+++ Test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test2
+//
+//  Created by Victor Alonso on 18/11/2018.
+//  Copyright © 2018 Global DPI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
